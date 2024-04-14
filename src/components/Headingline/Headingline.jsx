10f275edbb28f04d7b7ca579ea18bf868aacbf93
adityaappuse <adityashaji007@gmail.com>
2024-04-15 00:43:15 +0530

    Add .gitignore

---
+++ src/components/Headingline/Headingline.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './headingline.css'
+function Headingline() {
+  return (
+    <div className='main'>Norm OS</div>
+  )
+}
+export default Headingline;
---
+++ src/components/Page1/Page1.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './page1.css'
+ function Page1 () {
+  return (
+    <div className='main1'>
+      <section className='sub1'>
+       Builiding one byte,  time
+       
+       
+        <br />
+        <span className='links'>
+            <a href='https://sourceforge.net/projects/norm-os/files/'>Downloads</a>&nbsp;
+             <a href='https://github.com/adith-ts/norm-os'>Github</a>
+        </span>
+      </section>
+      <section className='subright'>
+        minds  at a
+      </section>
+    </div>
+  )
+}
+export default Page1;
